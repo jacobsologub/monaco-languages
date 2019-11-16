6ddea611b4e1edf45a274bb0209a50d770163f10
Jacob Sologub <jacobsologub@gmail.com>
2019-11-15 20:19:43 -0800

    Minor code clean clean-up/formatting inside soul.ts

--- src/soul/soul.ts
+++ src/soul/soul.ts
@@ -29,191 +29,173 @@ import ILanguage = monaco.languages.IMonarchLanguage;
 
 export const conf: IRichLanguageConfiguration = {
         comments: {
-                lineComment: '//',
-                blockComment: ['/*', '*/'],
+            lineComment: '//',
+            blockComment: ['/*', '*/'],
         },
         brackets: [
-                ['{', '}'],
-                ['[', ']'],
-                ['(', ')']
+            ['{', '}'],
+            ['[', ']'],
+            ['(', ')']
         ],
         autoClosingPairs: [
-                { open: '[', close: ']' },
-                { open: '{', close: '}' },
-                { open: '(', close: ')' },
-                { open: '\'', close: '\'', notIn: ['string', 'comment'] },
-                { open: '"', close: '"', notIn: ['string'] },
+            { open: '[', close: ']' },
+            { open: '{', close: '}' },
+            { open: '(', close: ')' },
+            { open: '\'', close: '\'', notIn: ['string', 'comment'] },
+            { open: '"', close: '"', notIn: ['string'] },
         ],
         surroundingPairs: [
-                { open: '{', close: '}' },
-                { open: '[', close: ']' },
-                { open: '(', close: ')' },
-                { open: '"', close: '"' },
-                { open: '\'', close: '\'' },
+            { open: '{', close: '}' },
+            { open: '[', close: ']' },
+            { open: '(', close: ')' },
+            { open: '"', close: '"' },
+            { open: '\'', close: '\'' },
         ],
         folding: {
-                markers: {
-                        start: new RegExp("^\\s*#pragma\\s+region\\b"),
-                        end: new RegExp("^\\s*#pragma\\s+endregion\\b")
-                }
+            markers: {
+                start: new RegExp("^\\s*#pragma\\s+region\\b"),
+                end: new RegExp("^\\s*#pragma\\s+endregion\\b")
+            }
         }
 };
 
 export const language = <ILanguage>{
     // Set defaultToken to invalid to see what you do not tokenize yet
-    defaultToken: 'invalid',
-    tokenPostfix: '.soul',
+    defaultToken: "invalid",
+    tokenPostfix: ".soul",
 
     keywords: [
-        'if', 'else',
-        'do', 'while', 'for', 'loop',
-        'break', 'continue', 'return',
-        'const', 'let', 'var',
-        'void', 'int', 'int32', 'int64',
-        'float', 'float32', 'float64', 'fixed',
-        'bool', 'true', 'false', 'string',
-        'struct', 'using', 'external',
-        'graph', 'processor', 'namespace',
-        'connection',
-        'event', 'import',
-        'try', 'catch', 'throw'
+        "if", "else",
+        "do", "while", "for", "loop",
+        "break", "continue", "return",
+        "const", "let", "var",
+        "void", "int", "int32", "int64",
+        "float", "float32", "float64", "fixed",
+        "bool", "true", "false", "string",
+        "struct", "using", "external",
+        "graph", "processor", "namespace",
+        "connection",
+        "event", "import",
+        "try", "catch", "throw"
     ],
 
     endpointKeywords: [
-            'input', 'output'
+        "input", "output"
     ],
 
     wrapClampKeywords: [
-        'wrap', 'clamp'
+        "wrap", "clamp"
     ],
 
     brackets: [
-        { open: '{', close: '}', token: 'delimiter.curly' },
-        { open: '[', close: ']', token: 'delimiter.square' },
-        { open: '(', close: ')', token: 'delimiter.parenthesis' },
-        { open: '<', close: '>', token: 'delimiter.angle' }
+        { open: "{", close: "}", token: "delimiter.curly" },
+        { open: "[", close: "]", token: "delimiter.square" },
+        { open: "(", close: ")", token: "delimiter.parenthesis" },
+        { open: "<", close: ">", token: "delimiter.angle" }
     ],
 
     operators: [
-        '!', '=', '??', '||', '&&', '|', '^', '&', '==', '!=', '<=', '>=', '<<',
-        '+', '-', '*', '/', '%', '!', '~', '++', '--', '+=',
-        '-=', '*=', '/=', '%=', '&=', '|=', '^=', '<<=', '>>=', '>>', '=>', '::'
+        "!", "=", "??", "?", "||", "&&", "|", "^", "&", "==", "!=", "<=", ">=", "<<",
+        "+", "-", "*", "/", "%", "!", "~", "++", "--", "+=",
+        "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", ">>", "=>", "::"
     ],
 
-    symbols:  /[\!\.:=><~&|+\-*\/%@#]+/,
-
-    // C# style strings
-    escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-
-    // The main tokenizer for our languages
+    symbols: "[\\!\\.:=><~&|+?\\-*\/%@#]+",
+    
     tokenizer: {
         root: [
-
-            // identifiers and keywords
-            [/\@?[a-zA-Z_]\w*/, {
+            ["@?[a-zA-Z_]\\w*", {
                 cases: {
-                    '@wrapClampKeywords': { token: '@rematch', next: '@possibleWrapClampKeyword' },
-                    '@endpointKeywords': { token: 'keyword.$0', next: '@endpointDefinition' },
-                    '@keywords': { token: 'keyword.$0', next: '@qualified' },
-                    '@default': { token: 'identifier', next: '@qualified' }
+                    "@wrapClampKeywords": { token: "@rematch", next: "@possibleWrapClampKeyword" },
+                    "@endpointKeywords": { token: "keyword.$0", next: "@endpointDefinition" },
+                    "@keywords": { token: "keyword.$0", next: "@qualified" },
+                    "@default": { token: "identifier", next: "@qualified" }
                 }
             }],
 
-            // Highlight the connection operator to make it clearer
-            ['->', 'keyword.$0'],
-
-            // whitespace
-            { include: '@whitespace' },
-
-            // brackets
-            [/[{}()\[\]]/, '@brackets'],
-
-            // operators
-            [/@symbols/, {
+            ["->", "keyword.$0"],
+            { include: "@whitespace" },
+            ["[{}()\\[\\]]", "@brackets"],
+            ["@symbols", {
                 cases: {
-                    '@operators': 'delimiter',
-                    '@default': ''
+                    "@operators": "delimiter",
+                    "@default": ""
                 }
             }],
 
-                        [/"/, { token: 'string.quote', next: '@string' }],
+            ["\"", { token: "string.quote", next: "@string" }],
 
-            // numbers
-            [/[0-9_]*\.[0-9_]+([eE][\-+]?\d+)?[fFdD]?/, 'number.float'],
-            [/0[xX][0-9a-fA-F_]+/, 'number.hex'],
-            [/0[bB][01_]+/, 'number.hex'],
-            [/[0-9_]+[fF]/, 'number.float'],
-            [/[0-9_]+[lL]?/, 'number'],
+            ["[0-9_]*.[0-9_]+([eE][-+]?\\d+)?[fFdD]?", "number.float"],
+            ["0[xX][0-9a-fA-F_]+", "number.hex"],
+            ["0[bB][01_]+", "number.hex"],
+            ["[0-9_]+[fF]", "number.float"],
+            ["[0-9_]+[lL]?", "number"],
 
-            // separators
-            [/[;,]/, 'delimiter'],
+            ["[;,]", "delimiter"]
         ],
 
         endpointDefinition: [
-            ['event', 'keyword.$0', ''],
-            ['stream', 'keyword.$0', ''],
-            [';', '', '@pop'],
-            ['{', '', '@endpointDefinition'],
-            [/[a-zA-Z_][\w]*/, {
+            ["event", "keyword.$0", ""],
+            ["stream", "keyword.$0", ""],
+            [";", "", "@pop"],
+            ["{", "", "@endpointDefinition"],
+            ["\"", { token: "string.quote", next: "@string" }],
+            ["[a-zA-Z_][\\w]*", {
                 cases: {
-                    '@keywords': { token: 'keyword.$0' },
-                    '@default': 'identifier'
+                    "@keywords": { token: "keyword.$0" },
+                    "@default": "identifier"
                 }
             }],
-            [/[{}()\[\]]/, '@brackets'],
-            [/,/, 'delimiter'],
-            [/@symbols/, {
+            ["[{}()\\[\\]]", "@brackets"],
+            [",", "delimiter"],
+            ["@symbols", {
                 cases: {
-                    '@operators': 'delimiter',
-                    '@default': ''
+                    "@operators": "delimiter",
+                    "@default": ""
                 }
             }],
-            [/[0-9_]+[lL]?/, 'number'],
+            ["[0-9_]+[lL]?", "number"]
         ],
 
         possibleWrapClampKeyword: [
-            ['wrap[ ]*\<', '@rematch', '@wrapClampKeyword'],
-            ['wrap', '', '@pop'],
-            ['clamp[ ]*\<', '@rematch', '@wrapClampKeyword'],
-            ['clamp', '', '@pop'],
-            ['', '', '@pop'],
+            ["wrap[ ]*<", "@rematch", "@wrapClampKeyword"],
+            ["wrap", "", "@pop"],
+            ["clamp[ ]*<", "@rematch", "@wrapClampKeyword"],
+            ["clamp", "", "@pop"],
+            ["", "", "@pop"]
         ],
 
         wrapClampKeyword: [
-            ['wrap', 'keyword.$0', '@pop'],
-            ['clamp', 'keyword.$0', '@pop'],
+            ["wrap", "keyword.$0", "@pop"],
+            ["clamp", "keyword.$0", "@pop"]
         ],
 
         qualified: [
-            [/[a-zA-Z_][\w]*/, {
-                cases: {
-                    '@keywords': { token: 'keyword.$0' },
-                    '@default': 'identifier'
+            ["[a-zA-Z_][\\w]*", {
+                "cases": {
+                    "@keywords": { token: "keyword.$0" },
+                    "@default": "identifier"
                 }
             }],
-            [/\./, 'delimiter'],
-            ['', '', '@pop'],
+            ["\\.", "delimiter"],
+            ["", "", "@pop"]
         ],
 
         comment: [
-            [/[^\/*]+/, 'comment'],
-            // [/\/\*/,    'comment', '@push' ],    // no nested comments :-(
-            ['\\*/', 'comment', '@pop'],
-            [/[\/*]/, 'comment']
+            ["[^\/*]+", "comment"],
+            ["\\*/", "comment", "@pop"],
+            ["[\/*]", "comment"]
         ],
 
-                string: [
-                        [/[^\\"]+/, 'string'],
-                        [/@escapes/, 'string.escape'],
-                        [/"/, { token: 'string.quote', next: '@pop' }]
-                ],
+        string: [
+            ["[^\\\"]+", "string"],
+            ["\"", { token: "string.quote", "next": "@pop" }]
+        ],
 
         whitespace: [
-            [/^[ \t\v\f]*#((r)|(load))(?=\s)/, 'directive.csx'],
-            [/^[ \t\v\f]*#\w.*$/, 'namespace.cpp'],
-            [/[ \t\v\f\r\n]+/, ''],
-            [/\/\*/, 'comment', '@comment'],
-            [/\/\/.*$/, 'comment'],
-        ],
-    },
+            ["[ \\t\\v\\f\\r\\n]+", ""],
+            ["\/\\*", "comment", "@comment"],
+            ["\/\/.*$", "comment"]
+        ]
+    }
 };
